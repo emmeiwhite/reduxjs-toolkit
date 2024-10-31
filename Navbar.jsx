c83db7a81e7b70e505fd2d0fb4503cb430829782
emmeiwhite <emmeiwhite@gmail.com>
2024-10-31 19:04:33 +0530

    Navar with CartIcon created - UI

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,8 +1,16 @@
+import { CartIcon } from './src/icons'
+
 export default function Navbar() {
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux Toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <div className="total-amount">0</div>
+          </div>
+        </div>
       </div>
     </nav>
   )
